c21a3e993921af01d79e10a5294aa61dd963d8a2
PecherskiyD <pecherskiy@vector-best.ru>
2026-02-11 09:25:03 +0700

    правка ProtectedRoute

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,23 +1,30 @@
 import { FC } from 'react';
 import { Preloader } from '@ui';
-import { Navigate } from 'react-router-dom';
 import { useSelector } from '../../services/store';
+import { getCookie } from '../../utils/cookie';
+import { useLocation, Navigate } from 'react-router-dom';
 
 export type TProtectedRouteProps = {
   children: React.ReactNode;
 };
 
 export const ProtectedRoute: FC<TProtectedRouteProps> = ({ children }) => {
-  const { user } = useSelector((state) => state.loginUser);
-  const isLoading = false;
-  const isAccess = !user;
+  const { loading, user } = useSelector((state) => state.loginUser);
+  const token = getCookie('accessToken');
+  const location = useLocation();
+  const isLoading = loading;
 
   if (isLoading) {
     return <Preloader />;
   }
 
-  if (isAccess) {
-    return <Navigate to='/' />;
+  if (!token && !user) {
+    return <Navigate to='/login' />;
+  }
+
+  if (user) {
+    const { from } = location.state || { from: { pathname: '/' } };
+    return <Navigate replace to={from} />;
   }
 
   return children;
